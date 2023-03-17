8f7b23385107193fd59f5e29277f7b40a60bb636
Lucas Bonanni <lbonanni@eventbrite.com>
2023-03-17 18:44:31 -0300

    First commit

---
+++ src/main/java/com/example/peya/PeyaApplication.java
@@ -0,0 +1,13 @@
+package com.example.peya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/peya/PeyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
